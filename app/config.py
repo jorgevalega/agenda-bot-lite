1d3e2a91d275ed005fca3b146c28943d7f317715
Jorge Valega <53570143+jorgevalega@users.noreply.github.com>
2025-06-25 10:01:46 -0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,14 @@
+# app/config.py
+
+# Configurações gerais
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_USER = "teu_usuario"
+DB_PASSWORD = "tua_senha"
+DB_NAME = "agenda_bot"
+
+# Modo: "LITE" ou "PREMIUM"
+MODO_SISTEMA = "LITE"
+
+# Limite de tarefas para usuários gratuitos
+LIMITE_TAREFAS_FREE = 15
